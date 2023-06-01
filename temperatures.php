bc58b22ccc3997ba4c5e219cd37113be6eea5457
Sergey Syrota <serg.syrota@gmail.com>
2023-06-01 08:25:26 -0500

    Adding adapter for RS485 dependency

--- temperatures.php
+++ temperatures.php
@@ -5,8 +5,12 @@
 *****/
 
 require_once __DIR__ . '/lib.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use \SyrotaAutomation\Gearman;
 
 $bridge = new Homebridge();
+$gm = new Gearman('rs485');
 
 $sensorsList = [
   'df73fc3d023cabe944fed8a9cca976a8954e802f4b53d37e3de2377fe8f9bc7a' => [ // Master bedroom thermostat
@@ -19,7 +23,8 @@ $sensorsList = [
     'fanId' => '57b720dd90f89d2ce869c04241a1c69acd6ee6a69c28a7601f597bb2b165f30a'
   ],
   '89cf8d0c778533abe54027d734ecdc80f2317c92fceb0412379c05bfaf6e1d3f' => [ // Dining room thermostat (shows currently selected temp sensor)
-    'fanId' => 'a86ffc334512b803abd5496f632ccc8c0ee154191068abc55c84e7d4d526db48'
+    'fanId' => 'a86ffc334512b803abd5496f632ccc8c0ee154191068abc55c84e7d4d526db48',
+    'equipmentMonitorDevice' => 'BrAcSens'
   ],
   '6fbcfec62287945c4ef32c8d07d226cac7cee46e98d30978ee279a12c46e7ebf' => [ // Dining room temperature (separate from the thermostat)
   ],
